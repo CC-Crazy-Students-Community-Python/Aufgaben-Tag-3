d71657acbaaf9a5f275d6a2e1df613d378ebbe7b
Sandro Simperl <sandrosimperl.cc@gmail.com>
2023-01-27 07:26:22 +0100

    update

--- mycolors.py
+++ mycolors.py
@@ -14,3 +14,27 @@ def gray( input ):
     return "\033[97m{}\033[00m" .format( input )
 def black( input ):
     return "\033[98m{}\033[00m" .format( input )
+
+def colors( color ):
+    match color:
+        case "red":  return "\\033[91m{ITS RED}\\033[00m"
+        case "green": return "\\033[92m{ITS GREEN}\\033[00m"
+        case "yellow": return "\\033[93m{ITS YELLOW}\\033[00m"
+        case "lightpurple": return "\\033[94m{ITS LIGHTPURPLE}\\033[00m"
+        case "purple": return "\\033[95m{ITS PURPLE}\\033[00m"
+        case "cyan": return "\\033[96m{ITS CYAN}\\033[00m"
+        case "gray": return "\\033[97m{ITS GRAY}\\033[00m"
+        case "black": return "\\033[98m{ITS BLACK}\\033[00m"
+		case other:
+			return "\\033[91m{ITS RED}\\033[00m"
+			return "\\033[92m{ITS GREEN}\\033[00m"
+			return "\\033[93m{ITS YELLOW}\\033[00m"
+			return "\\033[94m{ITS LIGHTPURPLE}\\033[00m"
+			return "\\033[95m{ITS PURPLE}\\033[00m"
+			return "\\033[96m{ITS CYAN}\\033[00m"
+			return "\\033[97m{ITS GRAY}\\033[00m"
+			return "\\033[98m{ITS BLACK}\\033[00m"
+
+
+if __name__ == "__main__":
+    doctest.testmod()
